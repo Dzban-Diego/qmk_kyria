5ac22a8dbc6431af965a2d84f1da05534b4f70e4
Klimczewski <62210828+Dzban-Diego@users.noreply.github.com>
2023-10-24 14:44:16 +0200

    refactor

--- keymap.c
+++ keymap.c
@@ -26,6 +26,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // Domyślna warswa colemak_dh
     [_COL] = LAYOUT(
         KC_ESC       , KC_Q         , KC_W         , KC_F         , KC_P         , KC_B         , /*===========, =============, =============, ============*/ KC_J         , KC_L         , KC_U        , KC_Y          , KC_SCLN      , KC_DEL       ,
         KC_TAB       , KC_A         , KC_R         , KC_S         , KC_T         , KC_G         , /*===========, =============, =============, ============*/ KC_M         , KC_N         , KC_E        , KC_I          , KC_O         , KC_BSPC      ,
@@ -33,43 +34,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /*===========, =============, =========== */ OSM(MOD_RALT), KC_LGUI      , OSM(MOD_RALT), MO(1)        , KC_LCTL      , TG(4)        , MO(3)        , LT(6, KC_SPC), MO(5)        , MO(2)
     ),
 
+    // Systemowa nawigacja
 	[_SYS_NAV] = LAYOUT(
 	    KC_TRNS      , SGUI(KC_1)   , SGUI(KC_2)   , SGUI(KC_3)   , SGUI(KC_4)   , SGUI(KC_5)   , /*===========, =============, =============, ============*/ KC_PGUP      , KC_HOME      , KC_UP       , KC_END        , LGUI(KC_PGUP), KC_DEL       ,
-	    TG(7)        , RGUI(KC_1)   , RGUI(KC_2)   , RGUI(KC_3)   , RGUI(KC_4)   , RGUI(KC_5)   , /*===========, =============, =============, ============*/ KC_PGDN      , KC_HASH      , KC_EQUAL    , KC_RABK       , KC_MINUS     , KC_BSPC      ,
-        KC_NO        , KC_LGUI      , KC_LSFT      , KC_LCTL      , KC_LALT      , KC_NO        , KC_TRNS      , KC_TRNS      , KC_TRNS      , KC_NO        , KC_NO        , MO(2)        , MO(3)       , MO(4)         , KC_LSFT      , KC_F12       ,
+	    KC_TRNS      , RGUI(KC_1)   , RGUI(KC_2)   , RGUI(KC_3)   , RGUI(KC_4)   , RGUI(KC_5)   , /*===========, =============, =============, ============*/ KC_PGDN      , LSFT(KC_8)   , LSFT(KC_2)  , KC_RABK       , KC_MINUS     , KC_BSPC      ,
+        KC_NO        , KC_LGUI      , KC_LSFT      , KC_LCTL      , KC_LALT      , KC_NO        , KC_TRNS      , KC_TRNS      , KC_TRNS      , KC_NO        , KC_NO        , KC_NO        , KC_NO       , KC_NO         , KC_LSFT      , KC_F12       ,
         /*===========, =============, =========== */ TG(0)        , KC_NO        , KC_NO        , KC_TRNS      , KC_TRNS      , KC_NO        , KC_TRNS      , KC_NO        , KC_NO        , KC_NO
     ),
 
-
+    // Media
 	[_MED] = LAYOUT(
-	    KC_NO        , KC_NO        , KC_BTN1      , KC_MS_U      , KC_BTN2      , KC_WH_U      , /*===========, =============, =============, ============*/ KC_NO        , KC_NO        , KC_NO       , KC_NO         , KC_NO        , KC_NO        ,
-	    KC_NO        , KC_NO        , KC_MS_L      , KC_MS_D      , KC_MS_R      , KC_WH_D      , /*===========, =============, =============, ============*/ SGUI(KC_PGUP), KC_MUTE      , KC_VOLD     , KC_VOLU, KC_BRIU, KC_NO,
-	    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /*====*/ KC_NO, KC_NO, SGUI(KC_PGDN),KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID, KC_PSCR,
-	    KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS
+	    KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , /*===========, =============, =============, ============*/ KC_NO        , KC_NO        , KC_NO       , KC_NO         , KC_NO        , KC_NO        ,
+	    KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , /*===========, =============, =============, ============*/ KC_NO        , KC_MUTE      , KC_VOLD     , KC_VOLU       , KC_BRIU      , KC_NO        ,
+	    KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_MPRV      , KC_MPLY     , KC_MNXT       , KC_BRID      , KC_PSCR      ,
+        /*===========, =============, =========== */ KC_NO        , KC_NO        , KC_NO        , KC_TRNS      , KC_TRNS      , KC_TRNS      , KC_TRNS      , KC_TRNS      , KC_NO        , KC_TRNS
     ),
 
-    [_NAV] = LAYOUT(KC_ESC, LALT(KC_TAB), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LALT(KC_ENT),             KC_PGUP, KC_HOME, KC_UP, KC_END, KC_NO, KC_DEL,
-    KC_TAB, KC_LGUI, KC_LALT, KC_LSFT,KC_LCTL, KC_APP,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, LCTL(KC_SLSH), KC_BSPC,
-    TG(0), KC_F12, OSM(MOD_RALT), KC_WBAK, KC_WFWD, LCA(KC_B), TG(0), KC_NO,               KC_NO, KC_TRNS, LCTL(KC_B), LCTL(KC_H), KC_TRNS, MO(10), LCTL(KC_DOT), KC_ENT, KC_NO, KC_NO, KC_NO, MO(1), KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO),
+    // Nawigacja
+    [_NAV] = LAYOUT(
+        KC_ESC       , LALT(KC_TAB) , LCTL(KC_X)   , LCTL(KC_C)   , LCTL(KC_V)   , LALT(KC_ENT) , /*===========, =============, =============, ============*/ KC_PGUP      , KC_HOME      , KC_UP       , KC_END        , KC_NO        , KC_DEL       ,
+        KC_TAB       , KC_LGUI      , KC_LALT      , KC_LSFT      , KC_LCTL      , KC_APP       , /*===========, =============, =============, ============*/ KC_PGDN      , KC_LEFT      , KC_DOWN     , KC_RGHT       , LCTL(KC_SLSH), KC_BSPC      ,
+        TG(0)        , KC_F12       , OSM(MOD_RALT), KC_WBAK      , KC_WFWD      , LCA(KC_B)    , TG(0)        , KC_NO        , KC_NO        , KC_TRNS      , LCTL(KC_B)   , LCTL(KC_H)   , KC_TRNS     , MO(10)        , LCTL(KC_DOT) , KC_ENT       ,
+        /*===========, =============, =========== */ KC_NO        , KC_NO        , KC_NO        , MO(1)        , KC_NO        , KC_NO        , KC_TRNS      , KC_TRNS      , KC_NO        , KC_NO
+    ),
 
+    // Numpad
 	[_NUM] = LAYOUT(
-	    KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                  KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_DEL,
-	    KC_TAB, KC_PPLS, KC_PAST, KC_PMNS, KC_PSLS, KC_NO,                                          KC_0, KC_4, KC_5, KC_6, KC_NO, KC_BSPC,
-	    KC_NO, KC_NO, KC_PCMM, KC_PDOT, KC_PEQL, KC_NO, KC_NO, KC_TRNS,             KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_ENT,
-	                                        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, MO(3), KC_SPC, KC_0, KC_PDOT),
+	    KC_ESC       , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , /*===========, =============, =============, ============*/ KC_NO        , KC_7         , KC_8         , KC_9         , KC_NO        , KC_DEL       ,
+	    KC_TAB       , KC_PPLS      , KC_PAST      , KC_PMNS      , KC_PSLS      , KC_NO        , /*===========, =============, =============, ============*/ KC_0         , KC_4         , KC_5         , KC_6         , KC_NO        , KC_BSPC      ,
+	    KC_NO        , KC_NO        , KC_PCMM      , KC_PDOT      , KC_PEQL      , KC_NO        , KC_NO        , KC_TRNS      , KC_NO        , KC_NO        , KC_NO        , KC_1         , KC_2         , KC_3         , KC_NO        , KC_ENT       ,
+        /*===========, =============, =========== */ KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_NO        , KC_TRNS      , MO(3)        , KC_SPC       , KC_0         , KC_PDOT
+    ),
 
-
+    // Specjalne
 	[_SPEC] = LAYOUT(
-	    KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                                                       KC_6, KC_7, KC_8, KC_9, KC_0, KC_PSCR,
-	    KC_SLSH, KC_BSLS, KC_COLN, KC_LT, KC_GT, KC_NO,                                             LSFT(KC_8), TD(TD_P1), TD(TD_P2), TD(TD_P3), KC_MINS, KC_BSPC,
-	    KC_LSFT, KC_NO, KC_QUOT, KC_DQUO, LSFT(KC_BSLS), KC_AMPR, TG(0), KC_NO,         KC_NO, KC_NO, LSFT(KC_2), KC_RPRN, KC_RCBR, KC_RBRC, KC_EQL, KC_ENT,
-	                                    KC_NO, KC_NO, KC_LALT, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO),
+        KC_GRV       , KC_1         , KC_2         , KC_3         , KC_4         , KC_5         , /*===========, =============, =============, ============*/ KC_6         , KC_7         , KC_8         , KC_9         , KC_0         , KC_PSCR      ,
+	    KC_SLSH      , KC_BSLS      , KC_COLN      , KC_LT        , KC_GT        , KC_NO        , /*===========, =============, =============, ============*/ LSFT(KC_8)   , TD(TD_P1)    , TD(TD_P2)    , TD(TD_P3)    , KC_MINS      , KC_BSPC      ,
+	    KC_LSFT      , KC_NO        , KC_QUOT      , KC_DQUO      , LSFT(KC_BSLS), KC_AMPR      , TG(0)        , KC_NO        , KC_NO        , KC_NO        , LSFT(KC_2)   , KC_RPRN      , KC_RCBR      , KC_RBRC      , KC_EQL       , KC_ENT       ,
+	    /*===========, =============, =========== */ KC_NO        , KC_NO        , KC_LALT      , KC_NO        , KC_NO        , KC_NO        , KC_TRNS      , KC_TRNS      , KC_NO        , KC_NO
+    ),
 
-	 [_num] = LAYOUT(
-        KC_TRNS,    SGUI(KC_1), SGUI(KC_2), SGUI(KC_3), SGUI(KC_4), SGUI(KC_5),                                     KC_PGUP, KC_HOME, KC_UP,    KC_END,     LGUI(KC_PGUP), KC_DEL,
-        KC_0,      KC_1, KC_2, KC_3 ,KC_4,  KC_5,                                     KC_6, KC_7, KC_8, KC_9,KC_0, KC_BSPC,
-        KC_NO,      RSFT(KC_MINS),    RSFT(KC_4),    KC_LCTL,    KC_LALT,    KC_NO,      KC_TRNS, KC_TRNS,   KC_TRNS, KC_NO, KC_NO,   MO(2),   MO(3),    MO(4),      KC_LSFT,        KC_F12,
-        TG(0), KC_NO, KC_NO, KC_TRNS, KC_TRNS,  KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO),
+    // numerki
+    [_num] = LAYOUT(
+        KC_TRNS     , SGUI(KC_1)    , SGUI(KC_2)   , SGUI(KC_3)   , SGUI(KC_4)   , SGUI(KC_5)   , /*===========, =============, =============, ============*/ KC_PGUP      , KC_HOME      , KC_UP        , KC_END       , LGUI(KC_PGUP), KC_DEL       ,
+        KC_0        , KC_1          , KC_2         , KC_3         , KC_4         , KC_5         , /*===========, =============, =============, ============*/ KC_6         , KC_7         , KC_8         , KC_9         , KC_0         , KC_BSPC      ,
+        KC_NO       , RSFT(KC_MINS) , RSFT(KC_4)   , KC_LCTL      , KC_LALT      , KC_NO        , KC_TRNS      , KC_TRNS      , KC_TRNS      , KC_NO        , KC_NO        ,   MO(2)      , MO(3)        , MO(4)        , KC_LSFT      , KC_F12       ,
+        /*===========, =============, =========== */ TG(0)        , KC_NO        , KC_NO        , KC_TRNS      , KC_TRNS      , KC_NO        , KC_TRNS      , KC_NO        , KC_NO        , KC_NO
+    ),
 };
 
 
